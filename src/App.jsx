3fde862b73aee82333f0c9a8a935eb9e76d25709
saideepthipadala <102957802+saideepthipadala@users.noreply.github.com>
2023-11-18 09:32:17 +0530

    Tailwind Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-function App() {
-    return (
-        <>
-            <p>HELLO</p>
-        </>
-    );
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-400">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
